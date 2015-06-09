e78097e7374b6a08a640ea7e5b27f62b8877536a
root <root@pan.nje.cn>
2015-06-09 10:12:43 -0400

    package

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ var appPort  = 80;
 var koa      = require('koa');
 var route    = require('koa-route');
 var $proxy   = require('koa-http-proxy')
-var app      = require("./lib/api-loader")();
+var koa      = require('koa')
+var apiLoader      = require("./lib/api-loader");
 var session  = require('koa-session');
 var dbConfig = require("./db-config.json");
 var DBLoader = require("./lib/db-loader");
@@ -13,6 +14,8 @@ dbLoader.initDBConnect(function(err,db){
 		console.log(err);
 		return;
 	}
+	var app = koa();
+	//app.use(apiLoader(app));
 	//数据库连接全局变量
 	global.dbPool = db;
 	if(isDebug){  
