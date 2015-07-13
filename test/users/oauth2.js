4e4491991081ddb0188818e0baf026f77ff2a82c
jim <jim@miniyun.cn>
2015-07-13 17:05:09 +0800

    first test case

--- test/users/oauth2.js
+++ test/users/oauth2.js
@@ -1,33 +1,33 @@
 var assert = require("assert")
 var co = require("co")
 var request = require("co-request")
-var host = "http://127.0.0.1:8030"
 describe('Users', function() {
     describe('oauth2/token', function() {
-        it('should return token', function(done) {
-            var options = {
-                url: host + "/api/v1/oauth2/token",
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                method:"POST",
-                body: {
-                    name: 'admin',
-                    password: 'admin',
-                    device_name: 'jim-pc-windows7',
-                    app_key: 'JsQCsjF3yr7KACyT',
-                    app_secret: 'bqGeM4Yrjs3tncJZ'
-                },
-                json:true
-            } 
-            co.wrap(function* () { 
-                var postResponse = yield request(options)
-  				var body = postResponse.body
-  				var type = body.token_type 
-  				assert.equal("bearer",type)  
-                done()
+        it('should return token', function(done) { 
+            var co = require('co')
+            co.wrap(function*(){  
+                var host = "http://127.0.0.1:8031"
+                var options = {
+                    url: host + "/api/v1/oauth2/token",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    method:"POST",
+                    body: {
+                        name: 'admin',
+                        password: 'admin',
+                        device_name: 'jim-pc-windows7',
+                        app_key: 'JsQCsjF3yr7KACyT',
+                        app_secret: 'bqGeM4Yrjs3tncJZ'
+                    },
+                    json:true
+                } 
+                var postResponse = yield request(options) 
+                var body = postResponse.body 
+                var type = body.token_type 
+                assert.equal("bearer",type)  
+                done() 
             })()
-            
         })
     });
 })
