4996ec854c992ff4ec3963510c61a1378eb0edff
jim <jim@miniyun.cn>
2015-07-13 17:41:39 +0800

    first test case

--- test/request/header.js
+++
@@ -1,9 +0,0 @@
-
-var request = require('../context').request;
-
-describe('req.header', function(){
-  it('should return the request header object', function(){
-    var req = request();
-    req.header.should.equal(req.req.headers);
-  })
-})
--- test/request/headers.js
+++
@@ -1,9 +0,0 @@
-
-var request = require('../context').request;
-
-describe('req.headers', function(){
-  it('should return the request header object', function(){
-    var req = request();
-    req.headers.should.equal(req.req.headers);
-  })
-})
