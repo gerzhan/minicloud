0a36cfd69630d5e2b1e73bf19aa90254e3243b5b
root <root@localhost.localdomain>
2015-07-14 09:09:00 -0400

    first test case

--- test/users/oauth2.js
+++ test/users/oauth2.js
@@ -1,12 +1,27 @@
 var assert = require("assert")
-var co = require("co")
 var request = require("co-request")
 describe('Users', function() {
     describe('oauth2/token', function() {
-        it('should return token', function(done) { 
+        it('should return token', function(done) {
             var co = require('co')
-            co.wrap(function*(){  
-                var host = "http://127.0.0.1:8033"
+            co.wrap(function*() {
+                var config = require("../config-test.json")
+                var server = yield require("../../lib/loader/server-loader")(config)
+                // request(server.listen(8033))
+                //     .post('/api/v1/oauth2/token')
+                //     .type('json')
+                //     .send({
+                //         name: 'admin',
+                //         password: 'admin',
+                //         device_name: 'ji1111m-pc-windows7',
+                //         app_key: 'JsQCsjF3yr7KACyT',
+                //         app_secret: 'bqGeM4Yrjs3tncJZ'
+                //     })
+                //     .end(function(err,res) {
+                //         console.log(res.body)
+                //     })
+                server.listen(8044)
+                var host = "http://127.0.0.1:8044"
                 var options = {
                     url: host + "/api/v1/oauth2/token",
                     headers: {
@@ -26,7 +41,7 @@ describe('Users', function() {
                 var body = postResponse.body 
                 var type = body.token_type 
                 assert.equal("bearer",type)  
-                done() 
+                done()
             })()
         })
     });
