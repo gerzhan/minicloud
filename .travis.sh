492b047a058746e98366fa007da81194fa077024
jim <jim@miniyun.cn>
2015-07-14 17:12:47 +0800

    node v0.12

--- .travis.sh
+++ .travis.sh
@@ -1,6 +1,8 @@
-if [ -e '/usr/local/bin/iojs' ]; then 
-	node ./test/index-test.js & 
-else 
-	node --harmony-generators ./test/index-test.js & 
-fi
+echo "1111"
+# if [ -e '/usr/local/bin/iojs' ]; then 
+# 	node ./test/index-test.js & 
+# else 
+# 	node --harmony-generators ./test/index-test.js & 
+# fi
+node ./test/index-test.js & 
 sleep 5
