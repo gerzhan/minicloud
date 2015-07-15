547d2c4e1867d63aae25216e7685efcf48442900
jim <jim@miniyun.cn>
2015-07-15 08:51:32 +0800

    Simplify code

--- development/development.sh
+++
@@ -1,5 +0,0 @@
-kill -9 $(ps aux | grep 'index-development' | awk '{print $2}')
-node ./index-development.js &
-sleep 5
-npm test
-kill -9 $(ps aux | grep 'index-development' | awk '{print $2}')
--- development/index-development.js
+++
@@ -1,8 +0,0 @@
-var config = require("./config-development.json")
-//start server 
-var co = require('co')
-co.wrap(function*(){ 
-	var server = yield require("../lib/loader/server-loader")(config)  
-	server.listen(config.port)
-	console.log(config.port+" is running!")	
-})()
