eb6d8ca05d208e7df9121965445765c976704a7a
jim <jim@miniyun.cn>
2015-07-16 09:45:12 +0800

    Simplify dbLoader

--- test/context.js
+++ test/context.js
@@ -4,6 +4,11 @@ var config = appConfig[process.env.NODE_ENV]
 if(isTravis){
 	config = appConfig["travis-ci"]
 } 
+/**
+ * Return test App
+ * @return {Koa}
+ * @api public
+ */
 exports.getApp = function*(){
 	var app = yield require("../lib/loader/app-loader")(config)
 	return app.listen()
