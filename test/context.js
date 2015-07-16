d67570bbd931f4db15a5cadfff1d377142b96fc2
jim <jim@miniyun.cn>
2015-07-16 09:17:26 +0800

    split test/travis model

--- test/context.js
+++ test/context.js
@@ -1,5 +1,9 @@
 var appConfig = require("../config.json")
+var isTravis = Boolean(process.env.CI)
 var config = appConfig[process.env.NODE_ENV]
+if(isTravis){
+	config = appConfig["travis-ci"]
+} 
 exports.getApp = function*(){
 	var app = yield require("../lib/loader/app-loader")(config)
 	return app.listen()
