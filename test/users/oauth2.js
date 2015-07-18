4d70164b3ab6f84d7f71b4e277af62d614e9450b
root <root@localhost.localdomain>
2015-07-18 05:56:00 +0000

    before hook start app service and ready data

--- test/users/oauth2.js
+++ test/users/oauth2.js
@@ -2,37 +2,43 @@ var assert = require("assert")
 var request = require("supertest")
 var co = require('co')
 var context = require("../context")
-describe('Users', function() { 
+describe('Users', function() {
+    var app = null
+    //before hook start app server,initialize data
+    before(function(done) {
+        co.wrap(function*() {
+            //start server
+            app = yield context.getApp()
+            //ready data
+            var MiniClient = require("../../lib/model/mini-client")
+            var MiniUser = require("../../lib/model/mini-user")
+            yield MiniClient.create(-1, "web client", "JsQCsjF3yr7KACyT", "bqGeM4Yrjs3tncJZ", "", 1, "web client")
+            yield MiniUser.create("admin", "admin")
+            return done()
+        })()
+    })
+
     describe('oauth2/token', function() {
         it('should return token', function(done) {
-            co.wrap(function*() {
-                //start server
-                var app = yield context.getApp()
-                //ready data
-                var MiniClient = require("../../lib/model/mini-client")
-                var MiniUser = require("../../lib/model/mini-user")
-                yield MiniClient.create(-1,"web client","JsQCsjF3yr7KACyT","bqGeM4Yrjs3tncJZ","",1,"web client")
-                yield MiniUser.create("admin","admin")
-                //test
-                request(app)
-                    .post('/api/v1/oauth2/token')
-                    .type('json')
-                    .timeout(10000)
-                    .send({
-                        name: 'admin',
-                        password: 'admin',
-                        device_name: 'ji1111m-pc-windows7',
-                        app_key: 'JsQCsjF3yr7KACyT',
-                        app_secret: 'bqGeM4Yrjs3tncJZ'
-                    })
-                    .expect(200)
-                    .end(function(err, res) {
-                        if (err) return done(err)
-                        res.should.have.header('Content-Type', 'application/json; charset=utf-8')
-                        res.body.token_type.should.equal('bearer')
-                        done()
-                    })
-            })()
+            //test
+            request(app)
+                .post('/api/v1/oauth2/token')
+                .type('json')
+                // .timeout(2000)
+                .send({
+                    name: 'admin',
+                    password: 'admin',
+                    device_name: 'ji1111m-pc-windows7',
+                    app_key: 'JsQCsjF3yr7KACyT',
+                    app_secret: 'bqGeM4Yrjs3tncJZ'
+                })
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) return done(err)
+                    res.should.have.header('Content-Type', 'application/json; charset=utf-8')
+                    res.body.token_type.should.equal('bearer')
+                    done()
+                })
         })
-    });
+    })
 })
