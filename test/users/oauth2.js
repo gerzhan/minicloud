65523c8cae9df13e50bf0cf48becb65a651280ea
root <root@localhost.localdomain>
2015-07-18 08:37:28 +0000

    oauth2/token test case

--- test/users/oauth2.js
+++ test/users/oauth2.js
@@ -4,12 +4,12 @@ var co = require('co')
 var context = require("../context")
 describe('Users', function() {
     var app = null
-    //before hook start app server,initialize data
+        //before hook start app server,initialize data
     before(function(done) {
         co.wrap(function*() {
             //start server
-            app = yield context.getApp() 
-            //ready data
+            app = yield context.getApp()
+                //ready data
             var modelApp = require("../../lib/model/app")
             var modelUser = require("../../lib/model/user")
             yield modelApp.create(-1, "web client", "JsQCsjF3yr7KACyT", "bqGeM4Yrjs3tncJZ", "", 1, "web client")
@@ -20,11 +20,9 @@ describe('Users', function() {
 
     describe('oauth2/token', function() {
         it('should return token', function(done) {
-            //test
             request(app)
                 .post('/api/v1/oauth2/token')
                 .type('json')
-                // .timeout(2000)
                 .send({
                     name: 'admin',
                     password: 'admin',
@@ -38,6 +36,72 @@ describe('Users', function() {
                     res.should.have.header('Content-Type', 'application/json; charset=utf-8')
                     res.body.token_type.should.equal('bearer')
                     done()
+                })
+        })
+        it('should return 401,user not existed or disabled', function(done) {
+            request(app)
+                .post('/api/v1/oauth2/token')
+                .type('json')
+                .send({
+                    name: 'admin1',
+                    password: 'admin',
+                    device_name: 'ji1111m-pc-windows7',
+                    app_key: 'JsQCsjF3yr7KACyT',
+                    app_secret: 'bqGeM4Yrjs3tncJZ'
+                })
+                .expect(401)
+                .end(function(err, res) {
+                    if (err) return done(err)
+                    res.body.error_description.should.equal('user not existed or disabled')
+                    done()
+                })
+        })
+        it('should return 401,incorrect password', function(done) {
+            request(app)
+                .post('/api/v1/oauth2/token')
+                .type('json')
+                .send({
+                    name: 'admin',
+                    password: 'admin1',
+                    device_name: 'ji1111m-pc-windows7',
+                    app_key: 'JsQCsjF3yr7KACyT',
+                    app_secret: 'bqGeM4Yrjs3tncJZ'
+                })
+                .expect(401)
+                .end(function(err, res) {
+                    if (err) return done(err)
+                    res.body.error_description.should.equal('incorrect password')
+                    done()
+                })
+        })
+        it('should return 409,lock user', function(done) {
+            //ready data
+            co.wrap(function*() {
+                var modelUserMeta = require("../../lib/model/user-meta")
+                yield modelUserMeta.create(1, "password_error_count", "6")
+            })()
+            request(app)
+                .post('/api/v1/oauth2/token')
+                .type('json')
+                .timeout(9000)
+                .send({
+                    name: 'admin',
+                    password: 'admin',
+                    device_name: 'ji1111m-pc-windows7',
+                    app_key: 'JsQCsjF3yr7KACyT',
+                    app_secret: 'bqGeM4Yrjs3tncJZ'
+                })
+                .expect(401)
+                .end(function(err, res) {
+                    if (err) return done(err)
+                    res.body.error_description.should.equal('user is locked,enter the wrong password over five times.please try again after 15 minutes')
+                    //reset data
+                    co.wrap(function*() {
+                        var modelUserMeta = require("../../lib/model/user-meta")
+                        yield modelUserMeta.create(1, "password_error_count", "0")
+                        done()
+                    })()
+                    
                 })
         })
     })
