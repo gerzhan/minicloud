b155954e357630f48e0d35910a0f842fe52fff3b
jim <jim@miniyun.cn>
2015-07-20 10:27:39 +0800

    mongoDB

--- test/users/oauth2.js
+++ test/users/oauth2.js
@@ -20,67 +20,66 @@ describe(protocol+' Users', function() {
     })
 
     describe(protocol+' oauth2/token', function() {
-        // it('should return token', function(done) {
-        //     request(app)
-        //         .post('/api/v1/oauth2/token')
-        //         .type('json')
-        //         .send({
-        //             name: 'admin',
-        //             password: 'admin',
-        //             device_name: 'ji1111m-pc-windows7',
-        //             app_key: 'JsQCsjF3yr7KACyT',
-        //             app_secret: 'bqGeM4Yrjs3tncJZ'
-        //         })
-        //         .expect(200)
-        //         .end(function(err, res) {
-        //             if (err) return done(err)
-        //             res.should.have.header('Content-Type', 'application/json; charset=utf-8')
-        //             res.body.token_type.should.equal('bearer')
-        //             done()
-        //         })
-        // })
-        // it(protocol+' should return 401,user not existed or disabled', function(done) {
-        //     request(app)
-        //         .post('/api/v1/oauth2/token')
-        //         .type('json')
-        //         .send({
-        //             name: 'admin1',
-        //             password: 'admin',
-        //             device_name: 'ji1111m-pc-windows7',
-        //             app_key: 'JsQCsjF3yr7KACyT',
-        //             app_secret: 'bqGeM4Yrjs3tncJZ'
-        //         })
-        //         .expect(401)
-        //         .end(function(err, res) {
-        //             if (err) return done(err)
-        //             res.body.error_description.should.equal('user not existed or disabled')
-        //             done()
-        //         })
-        // })
-        // it(protocol+' should return 401,incorrect password', function(done) {
-        //     request(app)
-        //         .post('/api/v1/oauth2/token')
-        //         .type('json')
-        //         .send({
-        //             name: 'admin',
-        //             password: 'admin1',
-        //             device_name: 'ji1111m-pc-windows7',
-        //             app_key: 'JsQCsjF3yr7KACyT',
-        //             app_secret: 'bqGeM4Yrjs3tncJZ'
-        //         })
-        //         .expect(401)
-        //         .end(function(err, res) {
-        //             if (err) return done(err)
-        //             res.body.error_description.should.equal('incorrect password')
-        //             done()
-        //         })
-        // })
+        it('should return token', function(done) {
+            request(app)
+                .post('/api/v1/oauth2/token')
+                .type('json')
+                .send({
+                    name: 'admin',
+                    password: 'admin',
+                    device_name: 'ji1111m-pc-windows7',
+                    app_key: 'JsQCsjF3yr7KACyT',
+                    app_secret: 'bqGeM4Yrjs3tncJZ'
+                })
+                .expect(200)
+                .end(function(err, res) {
+                    if (err) return done(err)
+                    res.should.have.header('Content-Type', 'application/json; charset=utf-8')
+                    res.body.token_type.should.equal('bearer')
+                    done()
+                })
+        })
+        it(protocol+' should return 401,user not existed or disabled', function(done) {
+            request(app)
+                .post('/api/v1/oauth2/token')
+                .type('json')
+                .send({
+                    name: 'admin1',
+                    password: 'admin',
+                    device_name: 'ji1111m-pc-windows7',
+                    app_key: 'JsQCsjF3yr7KACyT',
+                    app_secret: 'bqGeM4Yrjs3tncJZ'
+                })
+                .expect(401)
+                .end(function(err, res) {
+                    if (err) return done(err)
+                    res.body.error_description.should.equal('user not existed or disabled')
+                    done()
+                })
+        })
+        it(protocol+' should return 401,incorrect password', function(done) {
+            request(app)
+                .post('/api/v1/oauth2/token')
+                .type('json')
+                .send({
+                    name: 'admin',
+                    password: 'admin1',
+                    device_name: 'ji1111m-pc-windows7',
+                    app_key: 'JsQCsjF3yr7KACyT',
+                    app_secret: 'bqGeM4Yrjs3tncJZ'
+                })
+                .expect(401)
+                .end(function(err, res) {
+                    if (err) return done(err)
+                    res.body.error_description.should.equal('incorrect password')
+                    done()
+                })
+        })
         it(protocol+' should return 409,lock user', function(done) {
             //ready data
             co.wrap(function*() {
                 var modelUserMeta = require("../../lib/model/user-meta") 
-                var meta = yield modelUserMeta.create(1, "password_error_count", "6")
-                console.log({'metavalue2222':meta.value})
+                var meta = yield modelUserMeta.create(1, "password_error_count", "6") 
             })()
             request(app)
                 .post('/api/v1/oauth2/token')
