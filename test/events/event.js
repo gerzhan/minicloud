d24d228c1f82c7e8f04164eb6039b3a133031ed1
app@miniyun.cn <app@miniyun.cn>
2015-07-28 04:31:54 -0400

    create and modify api: 1. create login event 2. clean login events

--- test/events/event.js
+++ test/events/event.js
@@ -3,7 +3,7 @@ var request = require("supertest")
 var co = require('co')
 var context = require("../context")
 var protocol = process.env.ORM_PROTOCOL
-describe(protocol + ' log', function() {
+describe(protocol + ' event', function() {
     var app = null
     var accessToken = null
         //before hook start app server,initialize data
@@ -15,10 +15,8 @@ describe(protocol + ' log', function() {
                     //ready data
                 var modelApp = require("../../lib/model/app")
                 var modelUser = require("../../lib/model/user")
-                var modelLog = require("../../lib/model/log")
                 yield modelApp.create(-1, "web client", "JsQCsjF3yr7KACyT", "bqGeM4Yrjs3tncJZ", "", 1, "web client")
                 yield modelUser.create("admin", "admin")
-                yield modelLog.create(1, 1, "Xiaomi 2014011", "192.168.0.222", 'a:3:{s:6:"action";i:0;s:9:"device_id";s:1:"1";s:11:"device_type";s:1:"1";}', 0)
                 request(app)
                     .post('/api/v1/oauth2/token')
                     .type('json')
@@ -50,8 +48,21 @@ describe(protocol + ' log', function() {
             .expect(200)
             .end(function(err, res) {
                 if (err) return done(err)
-                res.should.have.header('Content-Type', 'application/json; charset=utf-8')
-                res.body.message.should.equal('192.168.0.222')
+                res.body.length.should.equal(1)
+                // res.body[0].message.should.equal('192.168.0.222')
+                done()
+            })
+    })
+    it("should clean user's all login logs", function(done) {
+        request(app)
+            .post('/api/v1/event/clean_login_events')
+            .type('json')
+            .set({
+                Authorization: 'Bearer ' + accessToken
+            })
+            .expect(200)
+            .end(function(err, res) {
+                if (err) return done(err)
                 done()
             })
     })
