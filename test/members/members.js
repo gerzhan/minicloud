9a7c8b86e42b1035a152ec3cd14ce0992f2dfee6
app@miniyun.cn <app@miniyun.cn>
2015-07-30 21:33:24 -0400

    test case members

--- test/members/members.js
+++ test/members/members.js
@@ -1,103 +1,88 @@
-var assert = require("assert")
-var request = require("supertest")
-var co = require('co')
+var request = require("co-supertest")
 var context = require("../context")
 var protocol = process.env.ORM_PROTOCOL
 describe(protocol + ' Members', function() {
     var app = null
     var accessToken = null
         //before hook start app server,initialize data
-    before(function(done) {
-        co.wrap(function*() {
-            //start server
-            app = yield context.getApp()
-                //ready data
-            var modelApp = require("../../lib/model/app")
-            var modelUser = require("../../lib/model/user")
-            var modelUserMeta = require("../../lib/model/user-meta")
-            yield modelApp.create(-1, "web client", "JsQCsjF3yr7KACyT", "bqGeM4Yrjs3tncJZ", "", 1, "web client")
-            var user = yield modelUser.create("admin", "admin")
-            yield modelUserMeta.create(user.id, 'email', 'app@miniyun.cn')
-            yield modelUserMeta.create(user.id, 'nick', 'jim')
-            yield modelUserMeta.create(user.id, 'phone', '+864000250057')
-            yield modelUserMeta.create(user.id, 'total_space', '1073741824')
-            yield modelUserMeta.create(user.id, 'is_admin', '1')  
-            request(app)
-                .post('/api/v1/oauth2/token')
-                .type('json')
-                .send({
-                    name: 'admin',
-                    password: 'admin',
-                    device_name: 'ji1111m-pc-windows7',
-                    app_key: 'JsQCsjF3yr7KACyT',
-                    app_secret: 'bqGeM4Yrjs3tncJZ'
-                })
-                .expect(200)
-                .end(function(err, res) { 
-                    if (err) return done(err)
-                    res.should.have.header('Content-Type', 'application/json; charset=utf-8')
-                    res.body.token_type.should.equal('bearer')
-                        //set access_token
-                    accessToken = res.body.access_token
-                    return done()
-                })
-        })()
+    before(function*(done) {
+        //start server
+        app = yield context.getApp()
+            //ready data
+        var modelApp = require("../../lib/model/app")
+        var modelUser = require("../../lib/model/user")
+        var modelUserMeta = require("../../lib/model/user-meta")
+        yield modelApp.create(-1, "web client", "JsQCsjF3yr7KACyT", "bqGeM4Yrjs3tncJZ", "", 1, "web client")
+        var user = yield modelUser.create("admin", "admin")
+        yield modelUserMeta.create(user.id, 'email', 'app@miniyun.cn')
+        yield modelUserMeta.create(user.id, 'nick', 'jim')
+        yield modelUserMeta.create(user.id, 'phone', '+864000250057')
+        yield modelUserMeta.create(user.id, 'total_space', '1073741824')
+        yield modelUserMeta.create(user.id, 'is_admin', '1')
+        var res = yield request(app)
+            .post('/api/v1/oauth2/token')
+            .type('json')
+            .send({
+                name: 'admin',
+                password: 'admin',
+                device_name: 'ji1111m-pc-windows7',
+                app_key: 'JsQCsjF3yr7KACyT',
+                app_secret: 'bqGeM4Yrjs3tncJZ'
+            })
+            .expect(200)
+            .end()
+        res.should.have.header('Content-Type', 'application/json; charset=utf-8')
+        res.body.token_type.should.equal('bearer')
+            //set access_token
+        accessToken = res.body.access_token
+        return done()
     })
 
-    it(protocol + ' should get user details', function(done) {
-        request(app)
+    it(protocol + ' should get user details', function*(done) {
+        var res = yield request(app)
             .post('/api/v1/members/get_my_account')
             .type('json')
             .set({
                 Authorization: 'Bearer ' + accessToken
             })
             .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err)
-                res.body.name.should.equal('admin')
-                done()
-            })
+            .end()
+        res.body.name.should.equal('admin')
+        done()
     })
-    it(protocol + ' should return 401', function(done) {
-        request(app)
+    it(protocol + ' should return 401', function*(done) {
+        var res = yield request(app)
             .post('/api/v1/members/get_my_account')
             .type('json')
             .set({
                 Authorization: 'Bearer 12234'
             })
             .expect(401)
-            .end(function(err, res) {
-                if (err) return done(err)
-                done()
-            })
+            .end()
+        done()
     })
-    it(protocol + ' should get members list', function(done) {
-        request(app)
+    it(protocol + ' should get members list', function*(done) {
+        var res = yield request(app)
             .post('/api/v1/members/get_list')
             .type('json')
             .set({
                 Authorization: 'Bearer ' + accessToken
             })
             .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err)
-                res.body[0].name.should.equal('admin')
-                done()
-            })
+            .end()
+
+        res.body[0].name.should.equal('admin')
+        done()
     })
-    it(protocol + ' should search certain a member', function(done) {
-        request(app)
+    it(protocol + ' should search certain a member', function*(done) {
+        var res = yield request(app)
             .post('/api/v1/members/search')
             .type('json')
             .set({
                 Authorization: 'Bearer ' + accessToken
             })
             .expect(200)
-            .end(function(err, res) {
-                if (err) return done(err)
-                    // console.log(res.body)
-                // res.body.name.should.equal('admin')
-                done()
-            })
+            .end()
+        done()
     })
 })
