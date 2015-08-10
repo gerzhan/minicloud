1098a9bd5e557d54425fa960d576ceada2116930
jim <jim@miniyun.cn>
2015-08-10 09:57:45 +0800

    app.js->100%

--- test/options/options.js
+++ test/options/options.js
@@ -1,15 +1,17 @@
 var request = require('co-supertest')
 var context = require('../context')
 var protocol = process.env.ORM_PROTOCOL
-
+var assert = require('assert')
 describe(protocol + ' options', function() {
     this.timeout(10000)
     var app = null
     var MiniOption = null
+    var MiniApp = null
     var user = null
     before(function*(done) {
         app = yield context.getApp()
         MiniOption = require('../../lib/model/option')
+        MiniApp = require('../../lib/model/app')
         return done()
     })
     it(protocol + ' option.js/create', function*(done) {
@@ -32,6 +34,17 @@ describe(protocol + ' options', function() {
         deviceType.should.equal(5) 
         deviceType =  helpers.getDeviceTypeByClientId('Lt7hPcA6nuX38FY4')
         deviceType.should.equal(6)
+        done()
+    })
+    it(protocol + ' app.js/getApp', function*(done) {
+        var app = yield MiniApp.getApp('1122')
+        assert.equal(app,null)
+        done()
+    })
+    it(protocol + ' app.js/create', function*(done) {
+        var app = yield MiniApp.create(-1,'test app','client_id','client_secret','',true,'')
+        var app1 = yield MiniApp.create(-1,'test12 app','client_id','client_secret','',false,'')
+        app1.id.should.equal(app.id)
         done()
     })
     
