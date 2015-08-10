b3409d1c979301e2675e01e5f61f475fb4d5594b
jim <jim@miniyun.cn>
2015-08-10 10:39:41 +0800

    user.js->100%

---
+++ test/testcase/user.js
@@ -0,0 +1,32 @@
+var request = require('co-supertest')
+var context = require('../context')
+var protocol = process.env.ORM_PROTOCOL
+var assert = require('assert')
+describe(protocol + ' user.js', function() {
+    this.timeout(10000)
+    var app = null 
+    var MiniUser = null 
+    before(function*(done) {
+        app = yield context.getApp()
+        MiniUser = require('../../lib/model/user') 
+        return done()
+    })
+    it(protocol + ' getById', function*(done) {
+        var user = yield MiniUser.getById(1234)
+        assert.equal(user,null)
+        done()
+    })    
+    it(protocol + ' create', function*(done) {
+        var user1 = yield MiniUser.create('admin','1234')
+        var user2 = yield MiniUser.create('admin','2345')
+        assert.equal(user2.id,user2.id)
+        done()
+    })  
+    it(protocol + ' remove', function*(done) {
+        var user1 = yield MiniUser.create('admin','1234')
+        yield MiniUser.remove('admin')
+        var user2 = yield MiniUser.getByName('admin')
+        assert.equal(user2,null)
+        done()
+    })
+})
