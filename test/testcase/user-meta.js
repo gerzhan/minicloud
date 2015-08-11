3850ab9e1ff5c7d512b5482aed4dd122167a130b
jim <jim@miniyun.cn>
2015-08-11 11:19:24 +0800

    user-meta.js->100%

--- test/testcase/user-meta.js
+++ test/testcase/user-meta.js
@@ -25,13 +25,36 @@ describe(protocol + ' user-meta.js', function() {
     it(protocol + ' updatePasswordErrorTimes', function*(done) {
         yield MiniUserMeta.updatePasswordErrorTimes(2)
         meta = yield MiniUserMeta.getByKey(2, 'password_error_count')
-        assert.equal(meta.value, '1') 
+        assert.equal(meta.value, '1')
         done()
     })
     it(protocol + ' resetPasswordErrorTimes', function*(done) {
         yield MiniUserMeta.updatePasswordErrorTimes(2)
         meta = yield MiniUserMeta.resetPasswordErrorTimes(2)
-        assert.equal(meta.value, '0') 
+        assert.equal(meta.value, '0')
         done()
+    })
+    it(protocol + ' isLocked', function*(done) {
+        var co = require('co')
+        var user = yield MiniUser.create('admin', 'admin')
+            //lock user
+        var meta = yield MiniUserMeta.create(user.id, 'password_error_count', '5')
+            //after 15 minutes
+        var now = new Date()
+        var afterTime = now.getTime() - 15 * 60 * 1000
+        var afterDate = new Date()
+        afterDate.setTime(afterTime)
+        var db = global.dbPool.db
+        db.driver.execQuery('update miniyun_user_metas set updated_at=? where id=?', [afterDate, meta.id], function(err, data) {
+            co.wrap(function*() {
+                //reset password_error_count=0
+                yield MiniUserMeta.isLocked(user.id)
+                var newMeta = yield MiniUserMeta.getByKey(user.id, 'password_error_count')
+                assert.equal(newMeta.value, '0')
+                done()
+            })()
+
+        })
+
     })
 })
