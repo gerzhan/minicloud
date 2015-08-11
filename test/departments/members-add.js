70fceb94677f3a2dae4bfa6d3299eb59c9a4a725
app@miniyun.cn <app@miniyun.cn>
2015-08-11 05:25:26 -0400

    modify assert

--- test/departments/members-add.js
+++ test/departments/members-add.js
@@ -10,17 +10,20 @@ describe(protocol + ' departments members add', function() {
     var user = null
     var MiniDepartment = null
     var uuid = null
+    var userId = null
+    var MiniUserDepartmentRelation = null
     before(function*(done) {
         app = yield context.getApp()
         var MiniApp = require('../../lib/model/app')
         MiniUser = require('../../lib/model/user')
         MiniUserMeta = require('../../lib/model/user-meta')
-        MiniUserDepartmentRelation = require('../../lib/model/user-group-relation')
+        MiniUserDepartmentRelation = require('../../lib/model/user-department-relation')
         var MiniDevice = require('../../lib/model/device')
         MiniDepartment = require('../../lib/model/department')
         yield MiniApp.create(-1, 'web client', 'JsQCsjF3yr7KACyT', 'bqGeM4Yrjs3tncJZ', '', 1, 'web client')
         user = yield MiniUser.create('admin', 'admin')
         uuid = user.uuid
+        userId = user.id
         yield MiniUserMeta.create(user.id,"is_admin",'1')
         yield MiniUserMeta.create(user.id,"nick",'Allis')
         yield MiniDevice.create(user, 'web client', 'JsQCsjF3yr7KACyT')
@@ -46,9 +49,9 @@ describe(protocol + ' departments members add', function() {
         return done()
     })
 
-    it(protocol + ' departments/membersAdd 200', function*(done) {
+    it(protocol + ' departments/members/add 200', function*(done) {
         var res = yield request(app)
-            .post('/api/v1/departments/membersAdd')
+            .post('/api/v1/departments/members/add')
             .type('json')
             .set({
                 Authorization: 'Bearer ' + accessToken
@@ -59,12 +62,13 @@ describe(protocol + ' departments members add', function() {
             })
             .expect(200)
             .end()
-        // res.body[0].nick.should.equal('Allis')
+        var memberList = yield MiniUserDepartmentRelation.getAllByDepartmentId(1)
+        memberList[0].id.should.equal(userId)
         done()
     })
-    it(protocol + ' departments/membersAdd 400', function*(done) {
+    it(protocol + ' departments/members/add 400', function*(done) {
         var res = yield request(app)
-            .post('/api/v1/departments/membersAdd')
+            .post('/api/v1/departments/members/add')
             .type('json')
             .set({
                 Authorization: 'Bearer ' + accessToken
@@ -77,9 +81,9 @@ describe(protocol + ' departments members add', function() {
             .end()
         done()
     })
-    it(protocol + ' departments/membersAdd 401', function*(done) {
+    it(protocol + ' departments/members/add 401', function*(done) {
         var res = yield request(app)
-            .post('/api/v1/departments/membersAdd')
+            .post('/api/v1/departments/members/add')
             .type('json')
             .set({
                 Authorization: 'Bearer 12234'
@@ -92,9 +96,9 @@ describe(protocol + ' departments members add', function() {
             .end()
         done()
     })
-    it(protocol + ' departments/membersAdd 409 department not exist', function*(done) {
+    it(protocol + ' departments/members/add 409 department_not_exist', function*(done) {
         var res = yield request(app)
-            .post('/api/v1/departments/membersAdd')
+            .post('/api/v1/departments/members/add')
             .type('json')
             .set({
                 Authorization: 'Bearer ' + accessToken
@@ -105,11 +109,12 @@ describe(protocol + ' departments members add', function() {
             })
             .expect(409)
             .end()
+            res.body.error.should.equal('department_not_exist')
         done()
     })
-    it(protocol + ' departments/membersAdd 409 member not exist', function*(done) {
+    it(protocol + ' departments/members/add 409 member_not_exist', function*(done) {
         var res = yield request(app)
-            .post('/api/v1/departments/membersAdd')
+            .post('/api/v1/departments/members/add')
             .type('json')
             .set({
                 Authorization: 'Bearer ' + accessToken
@@ -120,6 +125,7 @@ describe(protocol + ' departments members add', function() {
             })
             .expect(409)
             .end()
+            res.body.error.should.equal('member_not_exist')
         done()
     })
 })
