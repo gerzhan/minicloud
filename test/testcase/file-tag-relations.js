8a47418179d1838f209e8d0bea1b0c5ef227b1bb
app@miniyun.cn <app@miniyun.cn>
2015-08-12 04:56:48 -0400

    test/testcase/file-tag-relations

---
+++ test/testcase/file-tag-relations.js
@@ -0,0 +1,20 @@
+var request = require('co-supertest')
+var context = require('../context')
+var protocol = process.env.ORM_PROTOCOL
+var assert = require('assert')
+describe(protocol + ' file-tag-relation.js', function() {
+    this.timeout(10000)
+    var app = null 
+    var MiniFileTagRelation = null 
+    before(function*(done) {
+        app = yield context.getApp()
+        MiniFileTagRelation = require('../../lib/model/file-tag-relation')  
+        return done()
+    })
+    it(protocol + ' create', function*(done) {
+        var relaton1 = yield MiniFileTagRelation.create(1,1)
+        var relaton2 = yield MiniFileTagRelation.create(1,1) 
+        assert.equal(relaton1.id,relaton2.id)
+        done()
+    })      
+})
