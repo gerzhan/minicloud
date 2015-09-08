5ba943212dea266469eb6be5d179206a2dbcc1b0
jim <jim@miniyun.cn>
2015-09-08 14:56:52 +0800

    files/download files/thumbnail test case

---
+++ test/files/download.js
@@ -0,0 +1,81 @@
+var request = require('co-supertest')
+var context = require('../context')
+var protocol = process.env.ORM_PROTOCOL
+var assert = require('assert')
+describe(protocol + ' files/download', function() {
+    this.timeout(10000)
+    var app = null
+    var user = null
+    var device = null
+    before(function*(done) {
+        app = yield context.getApp()
+        var MiniApp = require('../../lib/model/app')
+        var MiniUser = require('../../lib/model/user')
+        yield MiniApp.create(-1, 'web client', 'JsQCsjF3yr7KACyT', 'bqGeM4Yrjs3tncJZ', '', 1, 'web client')
+        user = yield MiniUser.create('admin', 'admin')
+
+        var res = yield request(app)
+            .post('/api/v1/oauth2/token')
+            .type('json')
+            .send({
+                name: 'admin',
+                password: 'admin',
+                device_name: 'ji1111m-pc-windows7',
+                client_id: 'JsQCsjF3yr7KACyT',
+                client_secret: 'bqGeM4Yrjs3tncJZ'
+            })
+            .expect(200)
+            .end()
+            //set access_token
+        accessToken = res.body.access_token
+        var MiniDevice = require('../../lib/model/device')
+            //get current device
+        var devices = yield MiniDevice.getAllByUserId(user.id)
+        device = devices[0]
+        for (var i = 0; i < devices.length; i++) {
+            var item = devices[i]
+            if (item.client_id === 'JsQCsjF3yr7KACyT') {
+                device = item
+            }
+        }
+        return done()
+    })
+    it(protocol + ' files/download 400', function*(done) {
+        var res = yield request(app)
+            .get('/api/v1/files/download?access_token=' + accessToken)
+            .type('json')
+            .expect(400)
+            .end()
+        done()
+    })
+    it(protocol + ' files/download 401', function*(done) {
+        var res = yield request(app)
+            .get('/api/v1/files/download?access_token=1234')
+            .type('json')
+            .expect(401)
+            .end()
+        done()
+    })
+    it(protocol + ' files/download 409', function*(done) {
+        var res = yield request(app)
+            .get('/api/v1/files/download?access_token=' + accessToken + '&path=/zz/1.doc')
+            .type('json')
+            .expect(409)
+            .end()
+        res.body.error.should.equal('path_not_exist')
+        done()
+    })
+    it(protocol + ' files/download  302', function*(done) {
+        var MiniVersion = require('../../lib/model/version')
+        var MiniFile = require('../../lib/model/file')
+        var version = yield MiniVersion.create('X1234567', 1073741825, 'doc')
+        var filePath = '/home/doc/DOCX/201508/测试目录/测试A.doc'
+        yield MiniFile.createFile(device, filePath, version)
+
+        var res = yield request(app)
+            .get('/api/v1/files/download?access_token=' + accessToken + '&path=' + filePath)
+            .expect(302)
+            .end()
+        done()
+    })
+})
