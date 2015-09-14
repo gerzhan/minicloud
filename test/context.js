5ff847e9e230cb8166642c05eb5a391fc1415d87
jim <jim@miniyun.cn>
2015-09-14 13:42:09 +0800

    cov->100%

--- test/context.js
+++ test/context.js
@@ -1,9 +1,9 @@
 var config = require('./config')
-var isTravis = Boolean(process.env.CI) 
+var isTravis = Boolean(process.env.CI)
 global.timeout = 30000
 var client = require('./socket-io-client')
 process.env.ORM_PROTOCOL = process.env.ORM_PROTOCOL || 'sqlite'
-if(!isTravis){
+if (!isTravis) {
     var dbConfig = config[process.env.ORM_PROTOCOL]
     dbConfig.password = dbConfig.password || '123456'
 }
@@ -12,8 +12,32 @@ var initSocketClient = function(app) {
         return function(done) {
             var socket = client(app)
             socket.on('connect', function() {
-                done(null,socket)
+                done(null, socket)
             })
+        }
+    }
+    /**
+     * delete folder
+     * @param {String} sourcePath
+     * @param {String} aimPath   
+     * @return {Boolean}
+     * @api private
+     */
+var _deleteFolder = function(filePath) {
+        var path = require('path')
+        var fs = require('fs')
+        var files = []
+        if (fs.existsSync(filePath)) {
+            files = fs.readdirSync(filePath)
+            files.forEach(function(file, index) {
+                var curPath = path.join(filePath, file)
+                if (fs.statSync(curPath).isDirectory()) { // recurse
+                    _deleteFolder(curPath)
+                } else { // delete file
+                    fs.unlinkSync(curPath)
+                }
+            })
+            fs.rmdirSync(filePath)
         }
     }
     /**
@@ -26,10 +50,11 @@ exports.getApp = function*() {
         if (!global.app) {
             var app = yield require('../lib/loader/app-loader')(config)
             global.app = app.listen()
-            global.socket = yield initSocketClient(app) 
+            global.socket = yield initSocketClient(app)
+            _deleteFolder('./cache')
+            _deleteFolder('./data')
         }
         return global.app
-
     }
     /**
      * Create tables from models 
