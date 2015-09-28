8f0af9325c5a1f7ba2bafefff55516830c60c6d8
jim <jim@miniyun.cn>
2015-09-28 15:52:40 +0800

    install db support package

--- install.js
+++ install.js
@@ -1,9 +1,11 @@
+var path = require('path')
 var cli = require('cli')
 cli.parse({
     config: [false, 'Config path', 'path', './config.json']
 })
-cli.main(function(args, options) {
-    var config = require(options.config)
+cli.main(function(args, options) { 
+	var configPath = path.resolve(process.cwd(), options.config)
+    var config = require(configPath)
     var migration = require('./lib/migration')
     require('co').wrap(function*() {
         yield migration.run(config.database)
