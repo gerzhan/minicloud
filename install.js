fd460e51ce406b78f7a189c94b6d927379a32803
jim <jim@miniyun.cn>
2015-09-28 15:02:17 +0800

    database install support config path

--- install.js
+++ install.js
@@ -1,4 +1,11 @@
-var migration = require('./lib/migration')
-require('co').wrap(function*() {
-    yield migration.run()
-})()
+var cli = require('cli')
+cli.parse({
+    config: [false, 'Config path', 'path', './config.json']
+})
+cli.main(function(args, options) {
+    var config = require(options.config)
+    var migration = require('./lib/migration')
+    require('co').wrap(function*() {
+        yield migration.run(config.database)
+    })()
+})
